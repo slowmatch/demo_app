72e5a8337889dd0ce0348cd74e172c1118116f05
Aaron Smith <aaron@plotlocker.com>
2011-12-14 20:56:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2ae2a5a0148c17b90c484f858e728eb007fbc984ce809d4d7b0db7496778ba639d3771a286694313730a95e57dd2f2105ccaa5db0fef1acd91c5a0f760cfae78'
